e6e17277c8fbcdaf560c59c81e5b02d983de7887
AlexandrosFragkis <fragisalexgrr10@gmail.com>
2025-07-02 14:22:00 +0300

    Update app.py

--- kostologisi_app/app.py
+++ kostologisi_app/app.py
@@ -89,7 +89,7 @@ with st.sidebar:
     st.subheader("Ντουλάπα συρόμενη από 1,6μ. έως 4 συρτάρια (Εσωτερικά μελαμίνη)")
     for mat in def_material_reference:
         st.session_state.material_reference_prices[f"{mat} (συρόμενη)"] = st.number_input(
-            f"{mat} (συρόμενη)",
+            f"{mat} (αναφορά)",
             value=float(st.session_state.material_reference_prices.get(f"{mat} (συρόμενη)", def_material_reference[mat])),
             min_value=0.0,
             key=f"ref_slide_{mat}"
